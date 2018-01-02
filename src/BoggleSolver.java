9f1544628b9cc7e92b8840c41f8b562d24a23f83
Alex Lee <phparkle@gmail.com>
2018-01-02 13:28:08 +0800

    Initial commit

---
+++ src/BoggleSolver.java
@@ -0,0 +1,15 @@
+
+public class BoggleSolver {
+
+    public BoggleSolver(String[] dictionary) {
+    }
+
+    public Iterable<String> getAllValidWords(BoggleBoard board) {
+        return null;
+    }
+
+    public int scoreOf(String word) {
+        return 0;
+    }
+
+}
